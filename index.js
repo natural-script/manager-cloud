e75b303fd6083f521a08ebb87056a4ead600c97f
GeeekyBoy <ahmed__hassan@outlook.com>
2017-09-09 21:13:28 +0200

    Adding Device Form Detector

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const getFileSize = require('remote-file-size');
 const app = express();
+const device = require('express-device');
+app.use(device.capture());
 app.use(cors());
 app.use(bodyParser({
   limit: '50mb'
 }));
 const root = path.join(__dirname, 'assets');
 
-  app.use(staticGzip(/(framework-LiveVersion\.min\.html|db-manager\.min\.html)$/));
+app.use(staticGzip(/(framework-LiveVersion\.min\.html|db-manager\.min\.html)$/));
 
-  app.use(express.static(root));
+app.use(express.static(root));
 
-  app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 
-  app.post('/getFileSize', function (req, res) {
-    getFileSize(req.body.fileURL, function (err, o) {
-      res.send(String(o));
-    });
+app.post('/getFileSize', function (req, res) {
+  getFileSize(req.body.fileURL, function (err, o) {
+    res.send(String(o));
   });
+});
+
+app.get('/deviceForm', function (req, res) {
+  res.send(req.device.type);
+});
